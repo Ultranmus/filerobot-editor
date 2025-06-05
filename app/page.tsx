0370ef4911873ad9587893e92bfbc39fdc483b39
Ultranmus <mathpalhimanshu33@gmail.com>
2025-06-05 17:04:24 +0530

    add editor

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,112 @@
-import Image from "next/image";
+'use client'
+import dynamic from 'next/dynamic';
+import React, { useEffect, useState } from 'react';
 
-export default function Home() {
+import { StyleSheetManager } from 'styled-components';
+import isPropValid from '@emotion/is-prop-valid';
+
+const FilerobotImageEditor = dynamic(
+  () => import('react-filerobot-image-editor'),
+  { ssr: false }
+);
+
+export default function FilerobotImageEditorPage() {
+  const [isImgEditorShown, setIsImgEditorShown] = useState(false);
+
+  const openImgEditor = () => {
+    setIsImgEditorShown(true);
+  };
+
+  const closeImgEditor = () => {
+    setIsImgEditorShown(false);
+  };
+
+  const [editorProps, setEditorProps] = useState<null | {
+    TABS: any;
+    TOOLS: any;
+  }>(null);
+
+
+  useEffect(() => {
+    const loadEditor = async () => {
+      const { TABS, TOOLS } = await import('react-filerobot-image-editor');
+      setEditorProps({ TABS, TOOLS });
+    };
+
+    loadEditor();
+  }, []);
+
+  if (!editorProps) return null;
+
+  const { TABS, TOOLS } = editorProps;
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className='h-screen flex flex-col gap-2'>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <button onClick={openImgEditor}>Open Filerobot image editor</button>
+      {/* <StyleSheetManager shouldForwardProp={isPropValid}> */}
+      {isImgEditorShown && (
+        <FilerobotImageEditor
+          source="https://scaleflex.airstore.io/demo/stephen-walker-unsplash.jpg"
+          onSave={(editedImageObject, designState) => console.log('saved', editedImageObject, designState)}
+          onClose={closeImgEditor}
+          annotationsCommon={{
+            fill: '#000000',
+          }}
+          avoidChangesNotSavedAlertOnLeave={true}
+          Text={{ text: 'KUMBA...' }}
+          Rotate={{ angle: 90, componentType: 'slider' }}
+          Crop={{
+            presetsItems: [
+              {
+                titleKey: 'classicTv',
+                descriptionKey: '4:3',
+                ratio: 4 / 3,
+                // icon: CropClassicTv, // optional, CropClassicTv is a React Function component. Possible (React Function component, string or HTML Element)
+              },
+              {
+                titleKey: 'cinemascope',
+                descriptionKey: '21:9',
+                ratio: 21 / 9,
+                // icon: CropCinemaScope, // optional, CropCinemaScope is a React Function component.  Possible (React Function component, string or HTML Element)
+              },
+            ],
+            presetsFolders: [
+              {
+                titleKey: 'socialMedia', // will be translated into Social Media as backend contains this translation key
+
+                // icon: Social, // optional, Social is a React Function component. Possible (React Function component, string or HTML Element)
+                groups: [
+                  {
+                    titleKey: 'facebook',
+                    items: [
+                      {
+                        titleKey: 'profile',
+                        width: 180,
+                        height: 180,
+                        descriptionKey: 'fbProfileSize',
+                      },
+                      {
+                        titleKey: 'coverPhoto',
+                        width: 820,
+                        height: 312,
+                        descriptionKey: 'fbCoverPhotoSize',
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          }}
+          tabsIds={[TABS.ADJUST, TABS.ANNOTATE, TABS.WATERMARK, TABS.FILTERS, TABS.FINETUNE, TABS.RESIZE]} // or {['Adjust', 'Annotate', 'Watermark']}
+          defaultTabId={TABS.ANNOTATE} // or 'Annotate'
+          defaultToolId={TOOLS.TEXT} // or 'Text'
+          savingPixelRatio={4}
+          previewPixelRatio={window.devicePixelRatio}
+          disableSaveIfNoChanges={true} />
+      )}
+      {/* </StyleSheetManager> */}
     </div>
+
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  images: {
+    loader: "imgix",
+    path: "/",
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  transpilePackages: [
+    'react-filerobot-image-editor',
+    'filerobot-image-editor',
+    'konva',
+  ],
+  experimental: { esmExternals: 'loose', },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Avoid bundling canvas on server – Konva uses it in SSR context
+      config.externals = [
+        ...(config.externals || []),
+        {
+          canvas: 'commonjs canvas',
+        },
+      ];
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
